c39419596927e30f71d9fa5d9fdb6ee3971171c6
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-09-20 03:20:29 -0400

    Fix show explore hack club migrations (#11714)

--- db/migrate/20250830235107_remove_show_explore_hack_club_from_referral_programs.rb
+++ db/migrate/20250830235107_remove_show_explore_hack_club_from_referral_programs.rb
@@ -1,6 +1,6 @@
 class RemoveShowExploreHackClubFromReferralPrograms < ActiveRecord::Migration[7.2]
   def up
-    safety_assured { remove_column :referral_programs, :show_explore_hack_club }
+    safety_assured { remove_column :referral_programs, :show_explore_hack_club, if_exists: true }
   end
 
   def down
--- db/migrate/20250901045714_make_show_explore_hack_club_nullable_in_referral_programs.rb
+++
@@ -1,5 +0,0 @@
-class MakeShowExploreHackClubNullableInReferralPrograms < ActiveRecord::Migration[7.2]
-  def change
-    change_column_null :referral_programs, :show_explore_hack_club, true
-  end
-end
