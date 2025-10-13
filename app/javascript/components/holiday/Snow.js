ea00579ba5f3fbab3372b89f1e82ce9409219abf
Gary Tou <gary@garytou.com>
2025-10-14 00:03:42 +0100

    @emergenitro Update snow z-index (#9865)
    Supersedes https://github.com/hackclub/hcb/pull/8968 due to git rewrite.
    Original PR by @emergenitro
    
    ----
    
    ## Summary of the problem
    Just updated the snow to have a higher z-index as it is currently going
    behind the search bar in orgs. Here's an image before:
    
    ![image](https://github.com/user-attachments/assets/d1ecbb0e-ccac-4dde-a365-590cf2bc7ad8)
    
    and after:
    
    ![image](https://github.com/user-attachments/assets/a2f123a9-92f2-4f89-b9af-051f171758c7)
    
    
    Thanks and let me know if there are any issues :)
    
    ---------
    
    Co-authored-by: Karthik Sankar <49610482+emergenitro@users.noreply.github.com>
    Co-authored-by: yodalightsabr <yoda@yodacode.xyz>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/javascript/components/holiday/Snow.js
+++ app/javascript/components/holiday/Snow.js
@@ -7,5 +7,23 @@ const useSnow = createPersistedState('shallItSnow')
 export default function Snow() {
   const [snow] = useSnow(true)
 
-  return <>{snow ? <Snowfall /> : null}</>
+  return (
+    <>
+      {snow ? (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 9999,
+            pointerEvents: 'none',
+          }}
+        >
+          <Snowfall />
+        </div>
+      ) : null}
+    </>
+  )
 }
