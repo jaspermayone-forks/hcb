cfd984505f194bab6c12b6641a58045c9ff27386
Sam Poder <sampoder@berkeley.edu>
2025-11-10 11:37:39 -0800

    Rewrite `ReceiptPolicy#destroy?` (#10665)
    More readable, spun out of https://github.com/hackclub/hcb/pull/10663
    
    ---------
    
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/policies/receipt_policy.rb
+++ app/policies/receipt_policy.rb
@@ -2,16 +2,25 @@
 
 class ReceiptPolicy < ApplicationPolicy
   def destroy?
-    user&.admin? ||
-      (record&.receiptable&.event &&
-        OrganizerPosition.role_at_least?(user, record.receiptable.event, :member) &&
-        unlocked?
-      ) ||
-      # Checking if receiptable is nil prevents unauthorized
-      # deletion when user no longer has access to an org
-      (record&.receiptable.nil? && record&.user == user) ||
-      (record&.receiptable.instance_of?(Reimbursement::Expense) && record&.user == user && unlocked?) ||
-      (user && record&.receiptable.try(:card_grant)&.user == user && record.receiptable.card_grant.active?)
+    return false if record.nil?
+    return true if user.admin?
+
+    # the receipt is in receipt bin.
+    if record.receiptable.nil?
+      return record.user == user
+    end
+
+    # any members of events should be able to modify receipts.
+    if record.receiptable.event
+      return OrganizerPosition.role_at_least?(user, record.receiptable.event, :member) && unlocked?
+    end
+
+    # the receipt is on a reimbursement report. people making reports may not be in the organization.
+    if record.receiptable.instance_of?(Reimbursement::Expense)
+      return record.receiptable.report.user == user && unlocked?
+    end
+
+    return false
   end
 
   def link?
