ab04b3cc69bb5893b408f6a80044050cc7c22727
Mohamad Mortada <mmortada685@gmail.com>
2025-11-23 01:34:58 -0800

    Added creating org invites to v4 api (#10543)
    Co-authored-by: Gary Tou <gary@garytou.com>
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/controllers/api/v4/invitations_controller.rb
+++ app/controllers/api/v4/invitations_controller.rb
@@ -3,14 +3,47 @@
 module Api
   module V4
     class InvitationsController < ApplicationController
+      include SetEvent
+
       skip_after_action :verify_authorized, only: [:index]
       before_action :set_invitation, except: [:index]
+      before_action :set_api_event, only: [:create]
 
       def index
         @invitations = current_user.organizer_position_invites.pending
       end
 
-      def show; end
+      def show
+        authorize @invitation
+      end
+
+      def create
+        authorize @event
+
+        unless policy(@event).can_invite_user?
+          return render json: { error: "You are not authorized to invite users" }, status: :forbidden
+        end
+
+        if @event.organizer_positions.exists?(user: User.find_by(email: params[:email]))
+          return render json: { error: "User is already an organizer" }, status: :unprocessable_entity
+        end
+
+        if @event.organizer_position_invites.pending.exists?(email: params[:email])
+          return render json: { error: "User already has a pending invitation" }, status: :unprocessable_entity
+        end
+
+        service = OrganizerPositionInviteService::Create.new(event: @event, sender: current_user, user_email: params[:email], is_signee: false, role: params[:role], enable_spending_controls: params[:enable_spending_controls], initial_control_allowance_amount: params[:initial_control_allowance_amount])
+
+        @invitation = service.model
+
+        authorize @invitation
+
+        if service.run
+          render :show, status: :created
+        else
+          render json: { error: "Failed to create invitation" }, status: :unprocessable_entity
+        end
+      end
 
       def accept
         unless @invitation.accept(show_onboarding: false)
