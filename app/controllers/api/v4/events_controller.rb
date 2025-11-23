3924801a27e43d1ae651374795e20cfcd0476060
Mohamad Mortada <mmortada685@gmail.com>
2025-11-23 01:46:13 -0800

    v4 Transaction filtering (#10505)
    ## Summary of the problem
    Want to enable transaction filtering on hcb mobile
    
    
    
    ## Describe your changes
    added amount, tag, user, dates, and receipt filtering to the v4
    transactions api.
    
    ---------
    
    Co-authored-by: Gary Tou <gary@garytou.com>
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/controllers/api/v4/events_controller.rb
+++ app/controllers/api/v4/events_controller.rb
@@ -19,8 +19,11 @@ module Api
       def transactions
         authorize @event, :show_in_v4?
 
-        @settled_transactions = TransactionGroupingEngine::Transaction::All.new(event_id: @event.id).run
-        @pending_transactions = PendingTransactionEngine::PendingTransaction::All.new(event_id: @event.id).run
+        @settled_transactions = TransactionGroupingEngine::Transaction::All.new(filters).run
+        TransactionGroupingEngine::Transaction::AssociationPreloader.new(transactions: @settled_transactions, event: @event).run!
+
+        @pending_transactions = PendingTransactionEngine::PendingTransaction::All.new(filters).run
+        PendingTransactionEngine::PendingTransaction::AssociationPreloader.new(pending_transactions: @pending_transactions, event: @event).run!
 
         type_results = ::EventsController.filter_transaction_type(params[:type], settled_transactions: @settled_transactions, pending_transactions: @pending_transactions)
         @settled_transactions = type_results[:settled_transactions]
@@ -67,6 +70,41 @@ module Api
         @has_more = transactions.length > start_index + limit
 
         transactions.slice(start_index, limit)
+      end
+
+      def filters
+        filter_params = params.require(:filters).permit(
+          :search,
+          :tag_id,
+          :expenses,
+          :revenue,
+          :minimum_amount,
+          :maximum_amount,
+          :start_date,
+          :end_date,
+          :user_id,
+          :missing_receipts,
+          :category,
+          :merchant,
+          :order_by
+        )
+
+        return {
+          event_id: @event.id,
+          search: filter_params[:search].presence,
+          tag_id: filter_params[:tag_id].presence,
+          expenses: filter_params[:expenses].presence,
+          revenue: filter_params[:revenue].presence,
+          minimum_amount: filter_params[:minimum_amount].presence ? Money.from_amount(filter_params[:minimum_amount].to_f) : nil,
+          maximum_amount: filter_params[:maximum_amount].presence ? Money.from_amount(filter_params[:maximum_amount].to_f) : nil,
+          start_date: filter_params[:start_date].presence,
+          end_date: filter_params[:end_date].presence,
+          user: filter_params[:user_id] ? @event.users.find_by_public_id(filter_params[:user_id]) : nil,
+          missing_receipts: filter_params[:missing_receipts].present?,
+          category: filter_params[:category].presence,
+          merchant: filter_params[:merchant].presence,
+          order_by: filter_params[:order_by].presence
+        }
       end
 
     end
