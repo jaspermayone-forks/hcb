jaspermayone-forks/hcb

commit 7931e0a2df089d04a1c33484ba177540821452c5

Move contracts to a new `Contract` model (#12094)
## Summary of the problem
<!-- Why are these changes being made? What problem does it solve? Link
any related issues to provide more details. -->
Currently, contracts are in an `OrganizerPosition::Contract` model and
tightly coupled to `OrganizerPositionInvite`. With the new application
flow I'm working on, we'll need contracts to be abstracted more to
support a source from either an `Event::Application` or
`OrganizerPositionInvite`, since `OrganizerPositionInvite` will not
exist yet when the applicant is signing the contract during the
application flow.

## Describe your changes
<!-- Explain your thought process to the solution and provide a quick
summary of the changes. -->
Primarily, this PR adds a new `Contract` model that is mostly the same
as `OrganizerPosition::Contract`, with a couple differences:
- Has a polymorphic relationship `Contractable` instead of a
relationship directly to `OrganizerPositionInvite`. Only
`OrganizerPositionInvite` implements this right now, but in the
applications PR `Event::Application` will also implement it.
- A `type` column instead of a `purpose` column. The `purpose` column
was added in the original implementation of
`OrganizerPosition::Contract`, but it is not currently being used. This
PR adds this back as STI, and a future PR will implement better
automation for termination contracts

All code that references `OrganizerPosition::Contract` has been changed
to reference `Contract` instead, including the relations on `User` and
`Event`. A future PR will drop the `OrganizerPosition::Contract` model
and associated table.

In the database migration that creates this new table, steps have been
added that copy over current data from the
`organizer_position_contracts` table and renames item types for versions
stored in the PaperTrail `versions` table.

I've tested this PR using my own DocuSeal account in test mode, but it

app/controllers/contracts_controller.rb
@@ -1,0 +1,55 @@
+# frozen_string_literal: true
+
+class ContractsController < ApplicationController
+  before_action :set_contract, only: [:void, :resend_to_user, :resend_to_cosigner]
+
+  # This is only used for sending a contract for an OrganizerPositionInvite via
+  # the team page.
+  def create
+    @contract = Contract.new(contract_params)
+    authorize @contract, policy_class: ContractPolicy
+    @contract.save!
+    flash[:success] = "Contract sent successfully."
+    redirect_back(fallback_location: event_team_path(@contract.event))
+  end
+
+  def void
+    authorize @contract, policy_class: ContractPolicy
+    @contract.mark_voided!
+    flash[:success] = "Contract voided successfully."
+    redirect_back(fallback_location: event_team_path(@contract.event))
+  end
+
+  def resend_to_user
+    authorize @contract, policy_class: ContractPolicy
+
+    ContractMailer.with(contract: @contract).notify.deliver_later
+
+    flash[:success] = "Contract resent to user successfully."
+    redirect_back(fallback_location: event_team_path(@contract.event))
+  end
+
+  def resend_to_cosigner
+    authorize @contract, policy_class: ContractPolicy
+
+    if @contract.cosigner_email.present?
+      ContractMailer.with(contract: @contract).notify_cosigner.deliver_later
+      flash[:success] = "Contract resent to cosigner successfully."
+    else
+      flash[:error] = "This contract has no cosigner."
+    end
+
+    redirect_back(fallback_location: event_team_path(@contract.event))
+  end
+
+  private
+
+  def set_contract
+    @contract = Contract.find(params[:id])
+  end
+
+  def contract_params
+    params.require(:contract).permit(:type, :contractable_id, :contractable_type, :cosigner_email, :include_videos)
+  end
+
+end

app/controllers/organizer_position_invites_controller.rb
@@ -33,7 +33,7 @@ class OrganizerPositionInvitesController < ApplicationController
 
     if service.run
       if @invite.is_signee
-        OrganizerPosition::Contract.create!(organizer_position_invite: @invite, cosigner_email: invite_params[:cosigner_email].presence, include_videos: invite_params[:include_videos])
+        @invite.send_contract(cosigner_email: invite_params[:cosigner_email].presence, include_videos: invite_params[:include_videos])
       end
       flash[:success] = "Invite successfully sent to #{user_email}"
       redirect_to event_team_path @invite.event

app/models/organizer_position_invite.rb
@@ -62,6 +62,7 @@ class OrganizerPositionInvite < ApplicationRecord
 
   include FriendlyId
   include OrganizerPosition::HasRole
+  include Contractable
 
   include PublicActivity::Model
   tracked owner: proc{ |controller, record| controller&.current_user }, event_id: proc { |controller, record| record.event.id }, recipient: proc { |controller, record| record.user }, only: [:create]
@@ -76,8 +77,10 @@ class OrganizerPositionInvite < ApplicationRecord
   belongs_to :user
   belongs_to :sender, class_name: "User"
 
+  belongs_to :contract_user, foreign_key: :user_id, class_name: "User", inverse_of: :organizer_position_invites
+  belongs_to :contract_event, foreign_key: :event_id, class_name: "Event", inverse_of: :organizer_position_invites
+
   belongs_to :organizer_position, optional: true
-  has_many :organizer_position_contracts, class_name: "OrganizerPosition::Contract", dependent: :destroy
 
   validate :not_already_organizer
   validate :not_already_invited, on: :create
@@ -93,12 +96,12 @@ class OrganizerPositionInvite < ApplicationRecord
     end
   end
 
-  def organizer_position_contract
-    organizer_position_contracts.where.not(aasm_state: :voided).last
+  def contract
+    contracts.where.not(aasm_state: :voided).last
   end
 
   def pending_signature?
-    is_signee && organizer_position_contracts.where(aasm_state: :signed).none?
+    is_signee && contracts.where(aasm_state: :signed).none?
   end
 
   def deliver
@@ -204,6 +207,36 @@ class OrganizerPositionInvite < ApplicationRecord
 
   def signee?
     is_signee
+  end
+
+  def send_contract(cosigner_email: nil, include_videos: false)
+    ActiveRecord::Base.transaction do
+      Contract::FiscalSponsorship.create!(contractable: self, cosigner_email:, include_videos:, external_template_id: event.plan.contract_docuseal_template_id, prefills: { "public_id" => event.public_id, "name" => event.name, "description" => event.airtable_record&.[]("Tell us about your event") })
+      update!(is_signee: true)
+      organizer_position&.update(is_signee: true)
+
+      event.set_airtable_status("Documents sent")
+    end
+  end
+
+  def on_contract_signed(contract)
+    if contract.is_a?(Contract::FiscalSponsorship)
+      deliver
+
+      # Unfreeze the event if this is the first signed contract
+      if event.contracts.signed.count == 1
+        event.update!(financially_frozen: false)
+      end
+
+      organizer_position&.update!(fiscal_sponsorship_contract:)
+    end
+  end
+
+  def on_contract_voided(contract)
+    if contract.is_a?(Contract::FiscalSponsorship)
+      update(is_signee: false)
+      organizer_position&.update(is_signee: false, fiscal_sponsorship_contract: nil)
+    end
   end
 
   private

app/views/contract/_form.html.erb
@@ -1,0 +1,24 @@
+<%= form_with(model: contract, scope: :contract, local: true, data: { turbo: false }, url: contracts_path(@contract)) do |form| %>
+  <%= form.hidden_field :type, value: "Contract::FiscalSponsorship" %>
+  <%= form.hidden_field :contractable_id %>
+  <%= form.hidden_field :contractable_type %>
+
+  <p class="muted">
+    This will send <%= contract.user.name %> a fiscal sponsorship agreement
+    for <%= contract.event.name %>. <strong>If they are under 18, <%= contract.user.name %> will need to have a parent sign on their behalf.</strong>
+  <p>
+
+  <div class="field field--checkbox">
+    <%= form.check_box :include_videos %>
+    <%= form.label :include_videos, "Would you like us to include the onboarding videos?" %>
+  </div>
+
+  <div class="field">
+    <%= form.label :cosigner_email, "Cosigned by (email address)" %>
+    <%= form.text_field :cosigner_email %>
+  </div>
+
+  <div class="actions">
+    <%= form.submit "Send contract" %>
+  </div>
+<% end %>

app/views/organizer_position_invites/_organizer_position_invite.html.erb
@@ -16,7 +16,7 @@
 
     <div class="flex g1 items-center">
       <% if admin_signed_in? %>
-        <% if organizer_position_invite.organizer_position_contract&.pending? || organizer_position_invite.organizer_position_contract&.sent? %>
+        <% if organizer_position_invite.contract&.pending? || organizer_position_invite.contract&.sent? %>
           <div class="flex">
             <div class="line-height-0 tooltipped tooltipped--w" aria-label="Pending signature, click to manage this contract.">
               <div class="warning">
@@ -25,14 +25,14 @@
                     <%= inline_icon("clock", size: 24) %>
                   </div>
                   <div data-menu-target="content" class="menu__content menu__content--2 menu__content--compact">
-                    <%= link_to void_organizer_position_contract_path(organizer_position_invite.organizer_position_contract), data: { turbo_confirm: "Are you sure you want to void this contract?", turbo_method: :post } do %>
+                    <%= link_to void_contract_path(organizer_position_invite.contract), data: { turbo_confirm: "Are you sure you want to void this contract?", turbo_method: :post } do %>
                       Void this contract
                     <% end %>
-                    <%= link_to resend_to_user_organizer_position_contract_path(organizer_position_invite.organizer_position_contract), data: { turbo_method: :post } do %>
+                    <%= link_to resend_to_user_contract_path(organizer_position_invite.contract), data: { turbo_method: :post } do %>
                       Resend to the user
                     <% end %>
-                    <% if organizer_position_invite.organizer_position_contract.cosigner_email.present? %>
-                      <%= link_to resend_to_cosigner_organizer_position_contract_path(organizer_position_invite.organizer_position_contract), data: { turbo_method: :post } do %>
+                    <% if organizer_position_invite.contract.cosigner_email.present? %>
+                      <%= link_to resend_to_cosigner_contract_path(organizer_position_invite.contract), data: { turbo_method: :post } do %>
                         Resend to the cosigner
                       <% end %>
                     <% end %>
@@ -42,10 +42,10 @@
             </div>
           </div>
         <% else %>
-          <div class="line-height-0 tooltipped tooltipped--w" aria-label="<%= (organizer_position_invite.organizer_position_contract&.signed? ? "Contract signee, click to view the contract." : "No contract signature, click to send a contract.") %>">
+          <div class="line-height-0 tooltipped tooltipped--w" aria-label="<%= (organizer_position_invite.contract&.signed? ? "Contract signee, click to view the contract." : "No contract signature, click to send a contract.") %>">
             <div class="warning">
-              <% if organizer_position_invite.organizer_position_contract&.signed? %>
-                <%= link_to organizer_position_invite.organizer_position_contract.document || event_documents_path(event: organizer_position_invite.event), class: "success" do %>
+              <% if organizer_position_invite.contract&.signed? %>
+                <%= link_to organizer_position_invite.contract.document || event_documents_path(event: organizer_position_invite.event), class: "success" do %>
                   <%= inline_icon("docs-positive", size: 24) %>
                 <% end %>
               <% else %>
@@ -54,7 +54,7 @@
                 <% end %>
                 <section class="modal modal--scroll bg-snow" data-behavior="modal" role="dialog" id="send_contract_<%= organizer_position_invite.id %>">
                   <%= modal_header "Send a contract" %>
-                  <%= render "organizer_position_contract/form", organizer_position_contract: OrganizerPosition::Contract.new(organizer_position_invite_id: organizer_position_invite.id) %>
+                  <%= render "contract/form", contract: Contract::FiscalSponsorship.new(contractable: organizer_position_invite) %>
                 </section>
               <% end %>
             </div>

app/views/organizer_positions/_organizer_position.html.erb
@@ -104,31 +104,31 @@
     <p class="m0">
       Invited
       <%= relative_timestamp(organizer_position.organizer_position_invite.created_at) %>
-      <%= "as a contract signee" if organizer_position.signee? || organizer_position.organizer_position_invite&.organizer_position_contract.present? %>
+      <%= "as a contract signee" if organizer_position.signee? || organizer_position.organizer_position_invite&.contract.present? %>
       by <%= organizer_position.organizer_position_invite.sender.name %>
     </p>
     <% user_mention organizer_position.organizer_position_invite.sender %>
     <% if admin_signed_in? %>
-      <div class="line-height-0 tooltipped tooltipped--w" aria-label="<%= (organizer_position.organizer_position_invite.organizer_position_contract.present? ? organizer_position.organizer_position_invite.organizer_position_contract.signed? ? "Contract signee" : "Pending signature, click to manage this contract" : "No contract signature") %>">
+      <div class="line-height-0 tooltipped tooltipped--w" aria-label="<%= (organizer_position.organizer_position_invite.contract.present? ? organizer_position.organizer_position_invite.contract.signed? ? "Contract signee" : "Pending signature, click to manage this contract" : "No contract signature") %>">
         <div class="warning">
-          <% if organizer_position.organizer_position_invite.organizer_position_contract&.signed? %>
-            <%= link_to organizer_position.organizer_position_invite.organizer_position_contract.document || event_documents_path(event: organizer_position.event), class: "success", data: { turbo_frame: "_top" } do %>
+          <% if organizer_position.organizer_position_invite.contract&.signed? %>
+            <%= link_to organizer_position.organizer_position_invite.contract.document || event_documents_path(event: organizer_position.event), class: "success", data: { turbo_frame: "_top" } do %>
               <%= inline_icon("docs-positive", size: 24) %>
             <% end %>
-          <% elsif organizer_position.organizer_position_invite.organizer_position_contract&.present? %>
+          <% elsif organizer_position.organizer_position_invite.contract&.present? %>
             <div data-controller="menu" data-menu-placement-value="bottom-start">
               <div class="primary pointer" data-menu-target="toggle" data-action="click->menu#toggle click@document->menu#close keydown@document->menu#keydown">
                 <%= inline_icon("clock", size: 24) %>
               </div>
               <div data-menu-target="content" class="menu__content menu__content--2 menu__content--compact">
-                <%= link_to void_organizer_position_contract_path(organizer_position.organizer_position_invite.organizer_position_contract), data: { turbo_confirm: "Are you sure you want to void this contract?", turbo_method: :post } do %>
+                <%= link_to void_contract_path(organizer_position.organizer_position_invite.contract), data: { turbo_confirm: "Are you sure you want to void this contract?", turbo_method: :post } do %>
                   Void this contract
                 <% end %>
-                <%= link_to resend_to_user_organizer_position_contract_path(organizer_position.organizer_position_invite.organizer_position_contract), data: { turbo_method: :post } do %>
+                <%= link_to resend_to_user_contract_path(organizer_position.organizer_position_invite.contract), data: { turbo_method: :post } do %>
                   Resend to the user
                 <% end %>
-                <% if organizer_position.organizer_position_invite.organizer_position_contract.cosigner_email.present? %>
-                  <%= link_to resend_to_cosigner_organizer_position_contract_path(organizer_position.organizer_position_invite.organizer_position_contract), data: { turbo_method: :post } do %>
+                <% if organizer_position.organizer_position_invite.contract.cosigner_email.present? %>
+                  <%= link_to resend_to_cosigner_contract_path(organizer_position.organizer_position_invite.contract), data: { turbo_method: :post } do %>
                     Resend to the cosigner
                   <% end %>
                 <% end %>
@@ -140,7 +140,7 @@
             <% end %>
             <section class="modal modal--scroll bg-snow" data-behavior="modal" role="dialog" id="send_contract_<%= organizer_position.organizer_position_invite.id %>">
               <%= modal_header "Send a contract" %>
-              <%= render "organizer_position_contract/form", organizer_position_contract: OrganizerPosition::Contract.new(organizer_position_invite_id: organizer_position.organizer_position_invite.id) %>
+              <%= render "contract/form", contract: Contract::FiscalSponsorship.new(contractable: organizer_position.organizer_position_invite) %>
             </section>
           <% end %>
         </div>

commit 111c51f83bafd74ec69a71401251b35853e37b69

Only create Contracts through Contractable (#12251)
## Summary of the problem
<!-- Why are these changes being made? What problem does it solve? Link
any related issues to provide more details. -->

https://appsignal.com/hack-club/sites/6596247683eb67648f30f807/exceptions/incidents/2394

Bug introduced in #12094 - since `Contract` and
`OrganizerPositionInvite` are now decoupled, there are some fields of
the `Contract` model that need to be set using info from the
`Contractable`. Notably, the `prefills` jsonb includes event data that
must be included in the `Contract` before creation, since it is used in
the after create callback that sends the payload to Docuseal. While we
were doing this in the `send_contract` method on
`OrganizerPositionInvite`, we were not doing this when creating a
contract using the create action in `ContractsController` (used when
sending a contract on a pre-existing invite or organizer position),
causing the above bug.

## Describe your changes
<!-- Explain your thought process to the solution and provide a quick
summary of the changes. -->
While this could have been fixed by just grabbing the necessary data
from the `Contractable` and passing it to `Contract.new` in
`ContractsController`, it's better to maintain that contracts should
only be created using methods on the `Contractable`. This way, changes
can be easily made to what data is passed to the `Contract`, and any
extra code (such as updating the `OrganizerPosition` or Airtable can be
done in one place.

This does involve updating the `OrganizerPositionInvite` after it has
been sent and accepted - I've fixed a validation to not fail when this
happens, but it would also be possible to modify the code so that this
model is essentially "frozen" after being accepted, and only update the
`is_signee` field before accepting.

app/controllers/contracts_controller.rb
@@ -3,16 +3,6 @@
 class ContractsController < ApplicationController
   before_action :set_contract, only: [:void, :resend_to_user, :resend_to_cosigner]
 
-  # This is only used for sending a contract for an OrganizerPositionInvite via
-  # the team page.
-  def create
-    @contract = Contract.new(contract_params)
-    authorize @contract, policy_class: ContractPolicy
-    @contract.save!
-    flash[:success] = "Contract sent successfully."
-    redirect_back(fallback_location: event_team_path(@contract.event))
-  end
-
   def void
     authorize @contract, policy_class: ContractPolicy
     @contract.mark_voided!
@@ -46,10 +36,6 @@ class ContractsController < ApplicationController
 
   def set_contract
     @contract = Contract.find(params[:id])
-  end
-
-  def contract_params
-    params.require(:contract).permit(:type, :contractable_id, :contractable_type, :cosigner_email, :include_videos)
   end
 
 end

app/controllers/organizer_position_invites_controller.rb
@@ -4,7 +4,7 @@ class OrganizerPositionInvitesController < ApplicationController
   include SetEvent
   include ChangePositionRole
 
-  before_action :set_opi, only: [:show, :accept, :reject, :cancel, :resend]
+  before_action :set_opi, except: [:new, :create]
   before_action :set_event, only: [:new, :create]
   before_action :hide_footer, only: :show
 
@@ -109,6 +109,15 @@ class OrganizerPositionInvitesController < ApplicationController
     @invite.deliver
 
     flash[:success] = "Invite successfully resent to #{@invite.user.email}"
+    redirect_to event_team_path @invite.event
+  end
+
+  def send_contract
+    authorize @invite
+
+    @invite.send_contract(cosigner_email: params[:cosigner_email].presence, include_videos: params[:include_videos])
+
+    flash[:success] = "Contract successfully sent"
     redirect_to event_team_path @invite.event
   end
 

app/models/organizer_position_invite.rb
@@ -82,7 +82,7 @@ class OrganizerPositionInvite < ApplicationRecord
 
   belongs_to :organizer_position, optional: true
 
-  validate :not_already_organizer
+  validate :not_already_organizer, if: -> { event_changed? || user_changed? }
   validate :not_already_invited, on: :create
   validates :accepted_at, absence: true, if: -> { rejected_at.present? }
   validates :rejected_at, absence: true, if: -> { accepted_at.present? }
@@ -221,7 +221,7 @@ class OrganizerPositionInvite < ApplicationRecord
 
   def on_contract_signed(contract)
     if contract.is_a?(Contract::FiscalSponsorship)
-      deliver
+      deliver if organizer_position.nil?
 
       # Unfreeze the event if this is the first signed contract
       if event.contracts.signed.count == 1

app/policies/organizer_position_invite_policy.rb
@@ -37,6 +37,10 @@ class OrganizerPositionInvitePolicy < ApplicationPolicy
     admin_or_manager? && !record.signee?
   end
 
+  def send_contract?
+    user&.admin?
+  end
+
   private
 
   def admin_or_manager?

app/views/contract/_form.html.erb
@@ -1,24 +1,0 @@
-<%= form_with(model: contract, scope: :contract, local: true, data: { turbo: false }, url: contracts_path(@contract)) do |form| %>
-  <%= form.hidden_field :type, value: "Contract::FiscalSponsorship" %>
-  <%= form.hidden_field :contractable_id %>
-  <%= form.hidden_field :contractable_type %>
-
-  <p class="muted">
-    This will send <%= contract.user.name %> a fiscal sponsorship agreement
-    for <%= contract.event.name %>. <strong>If they are under 18, <%= contract.user.name %> will need to have a parent sign on their behalf.</strong>
-  <p>
-
-  <div class="field field--checkbox">
-    <%= form.check_box :include_videos %>
-    <%= form.label :include_videos, "Would you like us to include the onboarding videos?" %>
-  </div>
-
-  <div class="field">
-    <%= form.label :cosigner_email, "Cosigned by (email address)" %>
-    <%= form.text_field :cosigner_email %>
-  </div>
-
-  <div class="actions">
-    <%= form.submit "Send contract" %>
-  </div>
-<% end %>

app/views/organizer_position_invites/_contract_form.html.erb
@@ -1,0 +1,22 @@
+<%# locals: (opi:) %>
+
+<%= form_with(local: true, data: { turbo: false }, url: send_contract_organizer_position_invite_path(id: opi.id)) do |form| %>
+  <p class="muted">
+    This will send <%= opi.user.name %> a fiscal sponsorship agreement
+    for <%= opi.event.name %>. <strong>If they are under 18, <%= opi.user.name %> will need to have a parent sign on their behalf.</strong>
+  <p>
+
+  <div class="field field--checkbox">
+    <%= form.check_box :include_videos %>
+    <%= form.label :include_videos, "Would you like us to include the onboarding videos?" %>
+  </div>
+
+  <div class="field">
+    <%= form.label :cosigner_email, "Cosigned by (email address)" %>
+    <%= form.text_field :cosigner_email %>
+  </div>
+
+  <div class="actions">
+    <%= form.submit "Send contract" %>
+  </div>
+<% end %>

app/views/organizer_position_invites/_organizer_position_invite.html.erb
@@ -54,7 +54,7 @@
                 <% end %>
                 <section class="modal modal--scroll bg-snow" data-behavior="modal" role="dialog" id="send_contract_<%= organizer_position_invite.id %>">
                   <%= modal_header "Send a contract" %>
-                  <%= render "contract/form", contract: Contract::FiscalSponsorship.new(contractable: organizer_position_invite) %>
+                  <%= render "organizer_position_invites/contract_form", opi: organizer_position_invite %>
                 </section>
               <% end %>
             </div>

app/views/organizer_positions/_organizer_position.html.erb
@@ -140,7 +140,7 @@
             <% end %>
             <section class="modal modal--scroll bg-snow" data-behavior="modal" role="dialog" id="send_contract_<%= organizer_position.organizer_position_invite.id %>">
               <%= modal_header "Send a contract" %>
-              <%= render "contract/form", contract: Contract::FiscalSponsorship.new(contractable: organizer_position.organizer_position_invite) %>
+              <%= render "organizer_position_invites/contract_form", opi: organizer_position.organizer_position_invite %>
             </section>
           <% end %>
         </div>

app/views/organizer_positions/_organizer_position_row.html.erb
@@ -67,7 +67,7 @@
             <% end %>
             <section class="modal modal--scroll bg-snow" data-behavior="modal" role="dialog" id="send_contract_<%= organizer_position.organizer_position_invite.id %>">
               <%= modal_header "Send a contract" %>
-              <%= render "contract/form", contract: Contract::FiscalSponsorship.new(contractable: organizer_position.organizer_position_invite) %>
+              <%= render "organizer_position_invites/contract_form", opi: organizer_position.organizer_position_invite %>
             </section>
           <% end %>
         </div>

config/routes.rb
@@ -332,10 +332,11 @@ Rails.application.routes.draw do
     post "resend"
     member do
       post "change_position_role"
+      post "send_contract"
     end
   end
 
-  resources :contracts, only: [:create] do
+  resources :contracts, only: [] do
     member do
       post "void"
       post "resend_to_user"