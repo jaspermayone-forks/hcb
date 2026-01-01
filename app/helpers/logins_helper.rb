5e634359a0505b754947a0b64474eafbe415a045
Sam Poder <sampoder@berkeley.edu>
2026-01-01 16:23:32 -0500

    Higher quality image (#12510)

--- app/helpers/logins_helper.rb
+++ app/helpers/logins_helper.rb
@@ -6,7 +6,7 @@ module LoginsHelper
       name: "Assemble",
       time: "Summer 2022",
       slug: "assemble",
-      background: "linear-gradient(180deg, rgb(0 0 0 / 20%) 0%, rgba(0 0 0 / 40%) 100%), url('https://hc-cdn.hel1.your-objectstorage.com/s/v3/2d58273e5fbfd30f_image.png')"
+      background: "linear-gradient(180deg, rgb(0 0 0 / 20%) 0%, rgba(0 0 0 / 40%) 100%), url('https://hc-cdn.hel1.your-objectstorage.com/s/v3/d925f56a1bc5d118_189933158-9f00ceaf-7f61-4bef-9911-4cf4a14e0e4d.jpeg')"
     }
   ].map do |hackathon|
     hackathon[:url] = "/#{hackathon[:slug]}"
