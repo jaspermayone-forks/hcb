04a62be6718bc787f72a1ccf9967ee7cba887168
Sam Poder <sampoder@berkeley.edu>
2026-01-15 20:41:38 +0100

    Expand `EXPECTED_TWILIO_ERRORS` (#12646)
    engr-alerts is too much rn

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,8 +8,13 @@ class ApplicationJob < ActiveJob::Base
     def self.sidekiq_options(**); end
   end
 
+  # Twilio errors we expect and don't need to report:
+  # 21408, 21612: can't send text messages to certain countries (e.g. UK)
+  # 60410: user has been flagged for fraud by Twilio
+  EXPECTED_TWILIO_ERRORS = %w[21408 21612 60410].freeze
+
   discard_on(Twilio::REST::RestError) do |job, error|
-    Rails.error.report(error) unless error.message.include?("errors/21612") # we can't send text messages to the UK and other countries: https://www.twilio.com/docs/errors/21612
+    Rails.error.report(error) unless EXPECTED_TWILIO_ERRORS.any? { |code| error.message.include?("errors/#{code}") }
   end
 
 end
