848b65a43ee385ed9bbbe7dd7852cbbc1a3114c5
Mohamad Mortada <mmortada685@gmail.com>
2026-01-15 18:38:59 -0800

    Remove unused tag policy method "index" (#12656)
    I love taking up space for no reason

--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class TagPolicy < ApplicationPolicy
-  def index?
-    auditor? || reader?
-  end
-
   def create?
     OrganizerPosition.role_at_least?(user, record, :member)
   end
