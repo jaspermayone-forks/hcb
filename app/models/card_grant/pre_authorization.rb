b783df08e77862fb807b2faaece24a99443ee908
yodalightsabr <yoda@yodacode.xyz>
2026-01-19 15:08:50 -0800

    [Card Grants] Improve UI and fix pre-authorization bugs (#12677)
    ## Summary of the problem
    
    We're showing the wrong state to users when they're not authorized to
    spend.
    
    ## Describe your changes
    
    Fix the UI bug and provide more context to orgnaizers.

--- app/models/card_grant/pre_authorization.rb
+++ app/models/card_grant/pre_authorization.rb
@@ -84,8 +84,8 @@ class CardGrant
     def status_badge_type(organizer: false)
       return :muted if draft?
       return :pending if submitted?
-      return :success if approved? || (fraudulent? && !organizer)
-      return :error if fraudulent? && organizer
+      return :success if approved? || (fraudulent? && authorized? && !organizer)
+      return :error if (fraudulent? && authorized? && organizer) || (fraudulent? && unauthorized?)
       return :error if rejected?
 
       :muted
@@ -94,8 +94,9 @@ class CardGrant
     def status_text(organizer: false)
       return "Draft" if draft?
       return "Under review" if submitted?
-      return "Approved" if approved? || (fraudulent? && !organizer)
-      return "Flagged as fraudulent" if fraudulent? && organizer
+      return "Approved" if approved? || (fraudulent? && authorized? && !organizer)
+      return "Flagged as fraudulent" if (fraudulent? && authorized? && organizer) || (fraudulent? && unauthorized?)
+
       return "Rejected" if rejected?
 
       aasm_state.humanize
