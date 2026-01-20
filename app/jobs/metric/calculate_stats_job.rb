acc498c55a0256743c25a52de7ca708ee4cfa19a
Gary Tou <gary@garytou.com>
2026-01-20 14:21:04 -0800

    [Stats] Update correct stats metric (#12686)
    ## Summary of the problem
    
    `/stats` endpoint hasn't updated since December 2025.
    `first_or_initialize` doesn't choose the metric from the current year.
    This means we've been updating the wrong record.
    
    ## Describe your changes
    
     So, we use `instance`, which handles that.

--- app/jobs/metric/calculate_stats_job.rb
+++ app/jobs/metric/calculate_stats_job.rb
@@ -5,7 +5,7 @@ class Metric
     queue_as :metrics
 
     def perform
-      stats = Metric::Hcb::Stats.first_or_initialize
+      stats = Metric::Hcb::Stats.instance
 
       stats.populate!
     end
