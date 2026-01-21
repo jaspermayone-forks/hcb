21d23fa6385b91c6dba2155728290c1e108b6ef3
Sam Poder <sampoder@berkeley.edu>
2026-01-21 04:30:16 +0100

    Remove DAF disbursements (#12688)
    We have migrated away from these.

--- app/services/disbursement_service/nightly.rb
+++ app/services/disbursement_service/nightly.rb
@@ -11,33 +11,26 @@ module DisbursementService
         amount_cents = disbursement.amount
         memo = disbursement.transaction_memo
 
-        if disbursement.destination_event.increase_account_id != disbursement.source_event.increase_account_id
-          Rails.error.unexpected "DAF transfers not implemented yet"
-          next
-        else
-          # events are on the same Increase account
+        # FS Main -> FS Operating
+        ColumnService.post "/transfers/book",
+                           idempotency_key: "#{disbursement.id}_outgoing",
+                           amount: amount_cents,
+                           currency_code: "USD",
+                           sender_bank_account_id: ColumnService::Accounts::FS_MAIN,
+                           receiver_bank_account_id: ColumnService::Accounts::FS_OPERATING,
+                           description: memo
 
-          # FS Main -> FS Operating
-          ColumnService.post "/transfers/book",
-                             idempotency_key: "#{disbursement.id}_outgoing",
-                             amount: amount_cents,
-                             currency_code: "USD",
-                             sender_bank_account_id: ColumnService::Accounts::FS_MAIN,
-                             receiver_bank_account_id: ColumnService::Accounts::FS_OPERATING,
-                             description: memo
-
-          # FS Operating -> FS Main
-          ColumnService.post "/transfers/book",
-                             idempotency_key: "#{disbursement.id}_incoming",
-                             amount: amount_cents,
-                             currency_code: "USD",
-                             sender_bank_account_id: ColumnService::Accounts::FS_OPERATING,
-                             receiver_bank_account_id: ColumnService::Accounts::FS_MAIN,
-                             description: memo
-        end
-
-        disbursement.mark_in_transit!
+        # FS Operating -> FS Main
+        ColumnService.post "/transfers/book",
+                           idempotency_key: "#{disbursement.id}_incoming",
+                           amount: amount_cents,
+                           currency_code: "USD",
+                           sender_bank_account_id: ColumnService::Accounts::FS_OPERATING,
+                           receiver_bank_account_id: ColumnService::Accounts::FS_MAIN,
+                           description: memo
       end
+
+      disbursement.mark_in_transit!
     end
 
   end
