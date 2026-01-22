0e9a231d82315b2441149b0cc337b72776347f6f
Sam Poder <sampoder@berkeley.edu>
2026-01-22 09:14:35 +0100

    No reimbursements while onboarding (#12702)
    Closes https://github.com/hackclub/hcb/issues/12684

--- app/models/reimbursement/report.rb
+++ app/models/reimbursement/report.rb
@@ -113,7 +113,7 @@ module Reimbursement
       event :mark_submitted do
         transitions from: [:draft, :reimbursement_requested], to: :submitted do
           guard do
-            user.payout_method.present? && event && !exceeds_maximum_amount? && !below_minimum_amount? &&
+            user.payout_method.present? && !user.onboarding? && event && !exceeds_maximum_amount? && !below_minimum_amount? &&
               expenses.any? && !missing_receipts? && !event.financially_frozen? && expenses.none? { |e| e.amount.zero? } &&
               !mismatched_currency? && payout_method_allowed?
           end
