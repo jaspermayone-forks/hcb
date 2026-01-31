ef6a1aed5b3c47216c232130a10be18f768e0035
Manu Gurudath <manusvathgurudath@gmail.com>
2026-01-30 19:52:56 -0800

    [Command bar] Add empty state (#12790)
    creates a beautiful empty state instead of a weird gap when you type
    something which has returns no results
    
    
    <img width="631" height="297" alt="image"
    src="https://github.com/user-attachments/assets/916aa0a3-33f3-4c22-9ff6-b316225e7661"
    />

--- app/javascript/components/command_bar/CommandBar.js
+++ app/javascript/components/command_bar/CommandBar.js
@@ -9,6 +9,7 @@ import {
   useRegisterActions,
   Priority,
   useKBar,
+  useMatches,
 } from 'kbar'
 import { initalActions, adminActions, generateEventActions } from './actions'
 import { KBarInput } from './input'
@@ -67,6 +68,22 @@ const animatorStyle = {
   borderRadius: 'var(--radius-xl)',
   overflow: 'hidden',
   boxShadow: 'var(--shadow-modal)',
+}
+
+function EmptyState() {
+  const { results } = useMatches()
+
+  return (
+    results.length === 0 && (
+      <div className="text-center font-semibold pt-4 pb-5">
+        <img
+          src="/dino_leaping_for_money.svg"
+          className="dino-svg mb-4 mx-auto w-100 block max-w-40"
+        />
+        No results found
+      </div>
+    )
+  )
 }
 
 function SearchAndResults() {
@@ -230,6 +247,7 @@ function SearchAndResults() {
         searched={searched}
       />
       <RenderResults />
+      <EmptyState />
     </KBarAnimator>
   )
 }
