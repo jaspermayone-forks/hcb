d6df3c09c73a8f4e4742bb353439aa033cf16559
Sam Poder <sampoder@berkeley.edu>
2026-02-01 11:27:38 +0100

    Try messing with things on the `LedgerJournal` (#12827)

--- config/initializers/ledgerjournal.rb
+++ config/initializers/ledgerjournal.rb
@@ -3,4 +3,6 @@
 # Alias the ledgerjournal gem's top-level Ledger module so it doesn't
 # conflict with our own Ledger model.
 LedgerJournal = Ledger
+LedgerJournal::Transaction::Ledger = Ledger
+LedgerJournal::Ledger = Ledger
 Object.send(:remove_const, :Ledger)
