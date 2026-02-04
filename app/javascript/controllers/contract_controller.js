53ea187643cf79f69900aca5b3e5b1a1a4095730
Samuel Fernandez <me@sfernandez.dev>
2026-02-04 18:12:03 -0500

    Add fullscreen toggle for contract signing (#12892)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Contract signing on mobile or even a small laptop is not a great
    experience right now, since the text and inputs can be so small.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Refactors the docuseal embed into a partial, and adds a fullscreen
    toggle for contracts in the top-right corner of the contract.
    
    
    
    https://github.com/user-attachments/assets/4e17bb69-e367-443d-a3e6-d11077b73984

--- app/javascript/controllers/contract_controller.js
+++ app/javascript/controllers/contract_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static values = { advancePath: String }
-  static targets = ['button']
+  static targets = ['button', 'outerContainer', 'innerContainer']
 
   completed() {
     if (this.hasButtonTarget) {
@@ -14,5 +14,15 @@ export default class extends Controller {
 
   void() {
     window.location.href = '/'
+  }
+
+  toggleFullscreen() {
+    this.outerContainerTarget.classList.toggle('fixed')
+    this.outerContainerTarget.classList.toggle('relative')
+    this.outerContainerTarget.classList.toggle('w-screen')
+    this.outerContainerTarget.classList.toggle('h-screen')
+    this.innerContainerTarget.classList.toggle('max-h-[50vh]')
+    this.innerContainerTarget.classList.toggle('md:max-h-[75vh]')
+    this.innerContainerTarget.classList.toggle('max-h-screen')
   }
 }
