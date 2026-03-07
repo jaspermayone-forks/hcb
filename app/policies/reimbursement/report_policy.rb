7e283e8fe69ec488e97824005087ea48560182d4
yodalightsabr <yoda@yodacode.xyz>
2026-03-07 14:39:17 -0800

    [Reimbursements] Fix permissions bugs for sub-organizations (#13153)

--- app/policies/reimbursement/report_policy.rb
+++ app/policies/reimbursement/report_policy.rb
@@ -3,7 +3,7 @@
 module Reimbursement
   class ReportPolicy < ApplicationPolicy
     def new?
-      admin || team_member
+      admin || reader
     end
 
     def create?
@@ -11,7 +11,7 @@ module Reimbursement
     end
 
     def show?
-      admin || team_member || creator || auditor
+      admin || reader || creator || auditor
     end
 
     def wise_transfer_quote?
@@ -92,8 +92,8 @@ module Reimbursement
       record.event && OrganizerPosition.role_at_least?(user, record.event, :manager)
     end
 
-    def team_member
-      record.event&.users&.include?(user)
+    def reader
+      record.event && OrganizerPosition.role_at_least?(user, record.event, :reader)
     end
 
     def creator
