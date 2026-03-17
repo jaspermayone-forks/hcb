3e236fb33b73163c6002fbcca739c04b56b01d8c
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-03-16 23:46:54 -0400

    [Transfers] Cleanup index page (#13222)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    - fix new policy and utilize more
    - use `any?` instead of `size > 0`
    - remove card grant code
    - rearrange modals
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    Co-authored-by: yodalightsabr <yoda@yodacode.xyz>

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -144,7 +144,7 @@ class EventPolicy < ApplicationPolicy
   end
 
   def new_transfer?
-    admin_or_manager? && !record.demo_mode?
+    auditor_or_reader? && !record.demo_mode?
   end
 
   def g_suite_overview?
