67924e099d12103151906432d2467318bf4709c2
Gary Tou <gary@garytou.com>
2026-03-31 15:55:49 -0700

    Revert "[Reimbursement] Fix soft deletion of associated Expenses" (#13347)

--- app/models/reimbursement/report.rb
+++ app/models/reimbursement/report.rb
@@ -74,7 +74,7 @@ module Reimbursement
     monetize :amount_to_reimburse_cents, allow_nil: true, with_model_currency: :currency
     monetize :amount_cents, as: "amount", allow_nil: true, with_model_currency: :currency
     validates :maximum_amount_cents, numericality: { greater_than: 0 }, allow_nil: true
-    has_many :expenses, foreign_key: "reimbursement_report_id", inverse_of: :report, dependent: :destroy
+    has_many :expenses, foreign_key: "reimbursement_report_id", inverse_of: :report, dependent: :delete_all
     has_one :payout_holding, inverse_of: :report
     alias_attribute :report_name, :name
     attribute :name, :string, default: -> { "Expenses from #{Time.now.strftime("%B %e, %Y")}" }
