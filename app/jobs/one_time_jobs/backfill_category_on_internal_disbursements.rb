e7f794c30ea1dc55dd09c74902fb965bf3a0a114
yodalightsabr <yoda@yodacode.xyz>
2026-04-03 11:05:30 -0700

    [Accounting] Add internal transfer category for disbursements (#13398)
    ## Summary of the problem
    
    Disbursements are currently uncategorized.
    
    
    ## Describe your changes
    
    This PR creates the "Internal transfer" category and makes it the
    default for new disbursements. It also adds a one-time-job to backfill
    uncategorized disbursements as "Internal transfer".

---
+++ app/jobs/one_time_jobs/backfill_category_on_internal_disbursements.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module OneTimeJobs
+  class BackfillCategoryOnInternalDisbursements < ApplicationJob
+    def perform
+      disbursements = Disbursement.where(source_subledger_id: nil, destination_subledger_id: nil)
+      slug = "internal-transfer"
+      category = TransactionCategory.find_or_create_by!(slug:)
+
+      disbursements.where(source_transaction_category: nil).update(source_transaction_category: category)
+      disbursements.where(destination_transaction_category: nil).update(destination_transaction_category: category)
+
+      hcb_codes = []
+      disbursements.find_each(batch_size: 100) do |disbursement|
+        hcb_codes << disbursement.incoming_hcb_code
+        hcb_codes << disbursement.outgoing_hcb_code
+      end
+
+      CanonicalTransaction.where(hcb_code: hcb_codes).find_each(batch_size: 100) do |ct|
+        next unless ct.category.nil?
+
+        TransactionCategoryService
+          .new(model: ct)
+          .set!(
+            slug:,
+            assignment_strategy: "automatic"
+          )
+      end
+
+      CanonicalPendingTransaction.where(hcb_code: hcb_codes).find_each(batch_size: 100) do |cpt|
+        next unless cpt.category.nil?
+
+        TransactionCategoryService
+          .new(model: cpt)
+          .set!(
+            slug:,
+            assignment_strategy: "automatic"
+          )
+      end
+    end
+
+  end
+
+end
