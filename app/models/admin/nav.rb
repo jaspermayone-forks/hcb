d89a93326edb24b1716f71eb3c75faebe684828a
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-04-14 23:34:07 -0400

    [Admin] Update pages available in command bar and nav (#13465)
    closes #13412
    
    - replace `ReimbursementIcon` with `SvgIcon`
    - convert `() => (window.location.pathname = ` to `navigate(`
    - add missing tabs to command bar
    - add missing tabs to nav
    - add missing counts and count types to nav
    - fix unselected tab bugs in nav
    - add task highlighting to dropdown tabs in nav
    
    > [!NOTE]
    > - Airtable links are still only accessible by the command bar and not
    by the nav
    > - This PR does not fix "OPDRs" and "Common documents" being styled as
    non-admin pages
    
    ---------
    
    Co-authored-by: yodalightsabr <yoda@yodacode.xyz>

--- app/models/admin/nav.rb
+++ app/models/admin/nav.rb
@@ -121,10 +121,236 @@ module Admin
       )
     end
 
+    def spending
+      Section.new(
+        name: "Spending",
+        items: [
+          make_item(
+            name: "ACH Transfers",
+            path: ach_admin_index_path,
+            count: ->{ AchTransfer.pending.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Checks",
+            path: increase_checks_admin_index_path,
+            count: ->{ IncreaseCheck.pending.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Disbursements",
+            path: disbursements_admin_index_path,
+            count: ->{ Disbursement.reviewing.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "PayPal Transfers",
+            path: paypal_transfers_admin_index_path,
+            count: ->{ PaypalTransfer.pending.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Wires",
+            path: wires_admin_index_path,
+            count: ->{ Wire.pending.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Wise Transfers",
+            path: wise_transfers_admin_index_path,
+            count: ->{ WiseTransfer.pending.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Reimbursements",
+            path: reimbursements_admin_index_path,
+            count: ->{ Reimbursement::Report.reimbursement_requested.count },
+            count_type: :tasks
+          )
+        ]
+      )
+    end
+
+    def ledger
+      Section.new(
+        name: "Ledger",
+        items: [
+          make_item(
+            name: "Ledger",
+            path: ledger_admin_index_path,
+            count: ->{ CanonicalTransaction.not_stripe_top_up.unmapped.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Pending Ledger",
+            path: pending_ledger_admin_index_path,
+            count: ->{ CanonicalPendingTransaction.unsettled.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Raw Transactions",
+            path: raw_transactions_admin_index_path,
+            count: ->{ RawCsvTransaction.unhashed.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Intrafi Transactions",
+            path: raw_intrafi_transactions_admin_index_path,
+            count: ->{ RawIntrafiTransaction.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "HCB Codes",
+            path: hcb_codes_admin_index_path,
+            count: ->{ HcbCode.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Unknown Merchants",
+            path: unknown_merchants_admin_index_path,
+            count: ->{ Rails.cache.fetch("admin_unknown_merchants")&.length || 0 },
+            count_type: :records
+          ),
+          make_item(
+            name: "Audits",
+            path: admin_ledger_audits_path,
+            count: ->{ Admin::LedgerAudit.pending.count },
+            count_type: :tasks
+          ),
+        ]
+      )
+    end
+
+    def incoming_money
+      Section.new(
+        name: "Incoming Money",
+        items: [
+          make_item(
+            name: "Donations",
+            path: donations_admin_index_path,
+            count: ->{ Donation.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Recurring Donations",
+            path: recurring_donations_admin_index_path,
+            count: ->{ RecurringDonation.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Invoices",
+            path: invoices_admin_index_path,
+            count: ->{ Invoice.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Sponsors",
+            path: sponsors_admin_index_path,
+            count: ->{ Sponsor.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Check Deposits",
+            path: admin_check_deposits_path,
+            count: ->{ CheckDeposit.unprocessed.count },
+            count_type: :tasks
+          )
+        ]
+      )
+    end
+
+    def organizations
+      Section.new(
+        name: "Organizations",
+        items: [
+          make_item(
+            name: "Applications (HCB)",
+            path: applications_admin_index_path,
+            count: ->{ Event::Application.under_review.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Organizations",
+            path: events_admin_index_path,
+            count: ->{ Event.approved.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Organization Balances",
+            path: balances_admin_index_path,
+            count: ->{ Event.approved.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "OPDRs",
+            path: organizer_position_deletion_requests_path,
+            count: ->{ OrganizerPositionDeletionRequest.under_review.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Google Workspaces",
+            path: google_workspaces_admin_index_path,
+            count: ->{ GSuite.needs_ops_review.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Account Numbers",
+            path: account_numbers_admin_index_path,
+            count: ->{ Column::AccountNumber.count },
+            count_type: :records
+          )
+        ]
+      )
+    end
+
+    def payroll
+      Section.new(
+        name: "Payroll",
+        items: [
+          make_item(
+            name: "Employees",
+            path: employees_admin_index_path,
+            count: ->{ Employee.onboarding.count },
+            count_type: :tasks
+          ),
+          make_item(
+            name: "Payments",
+            path: employee_payments_admin_index_path,
+            count: ->{ Employee::Payment.paid.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "W9s",
+            path: admin_w9s_path,
+            count: ->{ W9.count },
+            count_type: :records
+          )
+        ]
+      )
+    end
+
     def misc
       Section.new(
         name: "Misc",
         items: [
+          make_item(
+            name: "Blazer",
+            path: blazer_path,
+            count: ->{ Blazer::Query.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Flipper",
+            path: flipper_path,
+            count: ->{ Flipper.features.count },
+            count_type: :records
+          ),
+          make_item(
+            name: "Common Documents",
+            path: common_documents_path,
+            count: ->{ Document.common.count },
+            count_type: :records
+          ),
           make_item(
             name: "Bank Accounts",
             path: bank_accounts_admin_index_path,
@@ -156,6 +382,12 @@ module Admin
             count_type: :records
           ),
           make_item(
+            name: "Stripe Cards",
+            path: stripe_cards_admin_index_path,
+            count: ->{ StripeCard.count },
+            count_type: :records
+          ),
+          make_item(
             name: "Card Designs",
             path: stripe_card_personalization_designs_admin_index_path,
             count: ->{ StripeCard::PersonalizationDesign.count },
@@ -165,12 +397,6 @@ module Admin
             name: "Emails",
             path: emails_admin_index_path,
             count: ->{ Ahoy::Message.count },
-            count_type: :records
-          ),
-          make_item(
-            name: "Unknown Merchants",
-            path: unknown_merchants_admin_index_path,
-            count: ->{ Rails.cache.fetch("admin_unknown_merchants")&.length || 0 },
             count_type: :records
           ),
           make_item(
@@ -202,174 +428,6 @@ module Admin
             path: new_teenagers_leaderboard_admin_index_path,
             count: ->{ 0 }, # I think this would be expensive to calculate
             count_type: :records,
-          )
-        ]
-      )
-    end
-
-    def payroll
-      Section.new(
-        name: "Payroll",
-        items: [
-          make_item(
-            name: "Employees",
-            path: employees_admin_index_path,
-            count: ->{ Employee.onboarding.count }
-          ),
-          make_item(
-            name: "Payments",
-            path: employee_payments_admin_index_path,
-            count: ->{ Employee::Payment.paid.count },
-            count_type: :records
-          ),
-          make_item(
-            name: "W9s",
-            path: admin_w9s_path,
-            count: ->{ W9.all.count },
-            count_type: :records
-          )
-        ]
-      )
-    end
-
-    def organizations
-      Section.new(
-        name: "Organizations",
-        items: [
-          make_item(
-            name: "Applications (HCB)",
-            path: applications_admin_index_path,
-            count: ->{ Event::Application.under_review.count }
-          ),
-          make_item(
-            name: "Organizations",
-            path: events_admin_index_path,
-            count: ->{ Event.approved.count },
-            count_type: :records
-          ),
-          make_item(
-            name: "Google Workspace Requests",
-            path: google_workspaces_admin_index_path,
-            count: ->{ GSuite.needs_ops_review.count }
-          ),
-
-          make_item(
-            name: "Account Numbers",
-            path: account_numbers_admin_index_path,
-            count: ->{ Column::AccountNumber.count },
-            count_type: :records
-          )
-        ]
-      )
-    end
-
-    def incoming_money
-      Section.new(
-        name: "Incoming Money",
-        items: [
-          make_item(
-            name: "Donations",
-            path: donations_admin_index_path,
-            count: ->{ 0 }
-          ),
-          make_item(
-            name: "Recurring Donations",
-            path: recurring_donations_admin_index_path,
-            count: ->{ 0 }
-          ),
-          make_item(
-            name: "Invoices",
-            path: invoices_admin_index_path,
-            count: ->{ 0 }
-          ),
-          make_item(
-            name: "Sponsors",
-            path: sponsors_admin_index_path,
-            count: ->{ 0 }
-          )
-        ]
-      )
-    end
-
-    def ledger
-      Section.new(
-        name: "Ledger",
-        items: [
-          make_item(
-            name: "Ledger",
-            path: ledger_admin_index_path,
-            count: ->{ CanonicalTransaction.not_stripe_top_up.unmapped.count }
-          ),
-          make_item(
-            name: "Pending Ledger",
-            path: pending_ledger_admin_index_path,
-            count: ->{ CanonicalPendingTransaction.unsettled.count },
-            count_type: :records
-          ),
-          make_item(
-            name: "Raw Transactions",
-            path: raw_transactions_admin_index_path,
-            count: ->{ RawCsvTransaction.unhashed.count }
-          ),
-          make_item(
-            name: "Intrafi Transactions",
-            path: raw_intrafi_transactions_admin_index_path,
-            count: ->{ RawIntrafiTransaction.count },
-            count_type: :records
-          ),
-          make_item(
-            name: "HCB codes",
-            path: hcb_codes_admin_index_path,
-            count: ->{ 0 },
-            count_type: :records
-          ),
-          make_item(
-            name: "Audits",
-            path: admin_ledger_audits_path,
-            count: ->{ Admin::LedgerAudit.pending.count }
-          ),
-        ]
-      )
-    end
-
-    def spending
-      Section.new(
-        name: "Spending",
-        items: [
-          make_item(
-            name: "ACHs",
-            path: ach_admin_index_path,
-            count: ->{ AchTransfer.pending.count }
-          ),
-          make_item(
-            name: "Checks",
-            path: increase_checks_admin_index_path,
-            count: ->{ IncreaseCheck.pending.count }
-          ),
-          make_item(
-            name: "Disbursements",
-            path: disbursements_admin_index_path,
-            count: ->{ Disbursement.reviewing.count }
-          ),
-          make_item(
-            name: "PayPal",
-            path: paypal_transfers_admin_index_path,
-            count: ->{ PaypalTransfer.pending.count }
-          ),
-          make_item(
-            name: "Wires",
-            path: wires_admin_index_path,
-            count: ->{ Wire.pending.count }
-          ),
-          make_item(
-            name: "Wise transfers",
-            path: wise_transfers_admin_index_path,
-            count: ->{ WiseTransfer.pending.count }
-          ),
-          make_item(
-            name: "Reimbursements",
-            path: reimbursements_admin_index_path,
-            count: ->{ Reimbursement::Report.reimbursement_requested.count }
           )
         ]
       )
