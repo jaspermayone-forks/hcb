8550a410978ba3aa27b86a30554973dbb21d8f49
Mohamad Mortada <mmortada685@gmail.com>
2026-04-16 17:19:58 -0700

    [v4][Breaking Change] Shallow route ACH transfer (#13367)
    ## Summary of the problem
    Another API I doubt any person uses right now which needs to be switched
    to use our new API standard
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- config/routes.rb
+++ config/routes.rb
@@ -689,7 +689,6 @@ Rails.application.routes.draw do
           end
 
           resources :disbursements, path: "transfers", only: [:create]
-          resources :ach_transfers, only: [:create]
 
           resources :donations, path: "donations", only: [:create] do
             collection do
@@ -746,6 +745,7 @@ Rails.application.routes.draw do
         resources :checks, only: [:index, :create, :show]
         resources :sponsors, only: [:index, :show, :create]
         resources :check_deposits, only: [:index, :show, :create]
+        resources :ach_transfers, only: [:create]
 
         get "stripe_terminal_connection_token", to: "stripe_terminal#connection_token"
 
