812bf28f6e16c3398399362a6e34e0adce6d43fc
Samuel Fernandez <me@sfernandez.dev>
2026-04-22 16:27:11 -0400

    [OrganizerPositionInvite] Don't update is_signee on accepted invites (#13512)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    https://appsignal.com/hack-club/sites/6596247683eb67648f30f807/exceptions/incidents/2569
    - if we try to send an contract to an existing OP, we get an error when
    we try to set is_signee to true on the OPI, which we shouldn't do
    anyways.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Add a check for if invite's been accepted
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/models/organizer_position_invite.rb
+++ app/models/organizer_position_invite.rb
@@ -229,7 +229,7 @@ class OrganizerPositionInvite < ApplicationRecord
       fs_contract.parties.create!(user:, role: :signee)
       fs_contract.parties.create!(external_email: cosigner_email, role: :cosigner) if cosigner_email.present?
 
-      update!(is_signee: true)
+      update!(is_signee: true) unless accepted?
       organizer_position&.update(is_signee: true, fiscal_sponsorship_contract: fs_contract)
     end
 
