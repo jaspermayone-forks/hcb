0240e4dfd441c9741b9348c290995b9f9e0be88a
Manu Gurudath <manusvathgurudath@gmail.com>
2026-04-22 13:32:10 -0700

    [v4 refresh token] Add model annotations (#13537)
    Adds the missing model annotations which were not included in the
    original PR

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -4,25 +4,28 @@
 #
 # Table name: api_tokens
 #
-#  id               :bigint           not null, primary key
-#  expires_in       :integer
-#  ip_address       :inet
-#  refresh_token    :string
-#  revoked_at       :datetime
-#  scopes           :string
-#  token_bidx       :string
-#  token_ciphertext :text
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  application_id   :bigint
-#  user_id          :bigint           not null
+#  id                       :bigint           not null, primary key
+#  expires_in               :integer
+#  ip_address               :inet
+#  refresh_token            :string
+#  refresh_token_bidx       :text
+#  refresh_token_ciphertext :text
+#  revoked_at               :datetime
+#  scopes                   :string
+#  token_bidx               :string
+#  token_ciphertext         :text
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  application_id           :bigint
+#  user_id                  :bigint           not null
 #
 # Indexes
 #
-#  index_api_tokens_on_application_id  (application_id)
-#  index_api_tokens_on_ip_address      (ip_address)
-#  index_api_tokens_on_token_bidx      (token_bidx) UNIQUE
-#  index_api_tokens_on_user_id         (user_id)
+#  index_api_tokens_on_application_id       (application_id)
+#  index_api_tokens_on_ip_address           (ip_address)
+#  index_api_tokens_on_refresh_token_bidx   (refresh_token_bidx) UNIQUE
+#  index_api_tokens_on_token_bidx           (token_bidx) UNIQUE
+#  index_api_tokens_on_user_id              (user_id)
 #
 # Foreign Keys
 #
