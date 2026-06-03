42f4bc977bdb5c58eb315956b545a6bdc05d02b3
Manu Gurudath <manusvathgurudath@gmail.com>
2026-06-03 11:33:44 -0700

    [Admin] Add checkbox to exclude reimbursements to transfers (#13826)
    <img width="490" height="250" alt="image"
    src="https://github.com/user-attachments/assets/93adf83f-0953-46ef-a805-f718810bf3fa"
    />
    
    Closes https://github.com/hackclub/hcb/issues/13819 by adding an
    "Exclude reimbursements" checkbox

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -447,6 +447,7 @@ class AdminController < Admin::BaseController
     @per = params[:per] || 20
     @q = params[:q].presence
     @pending = params[:pending] == "1" ? true : nil
+    @exclude_reimbursements = params[:exclude_reimbursements] == "1" ? true : nil
 
     @event_id = params[:event_id].presence
 
@@ -476,6 +477,7 @@ class AdminController < Admin::BaseController
     end
 
     relation = relation.pending if @pending
+    relation = relation.where.missing(:reimbursement_payout_holding) if @exclude_reimbursements
 
     @count = relation.count
     @ach_transfers = relation.page(@page).per(@per).order(
@@ -689,7 +691,12 @@ class AdminController < Admin::BaseController
   def increase_checks
     @page = params[:page] || 1
     @per = params[:per] || 20
-    @checks = IncreaseCheck.page(@page).per(@per).order(
+    @exclude_reimbursements = params[:exclude_reimbursements] == "1" ? true : nil
+
+    relation = IncreaseCheck.all
+    relation = relation.where.missing(:reimbursement_payout_holding) if @exclude_reimbursements
+
+    @checks = relation.page(@page).per(@per).order(
       Arel.sql("aasm_state = 'pending' DESC"),
       "created_at desc"
     )
@@ -729,6 +736,7 @@ class AdminController < Admin::BaseController
     @page = params[:page] || 1
     @per = params[:per] || 20
     @q = params[:q].presence
+    @exclude_reimbursements = params[:exclude_reimbursements] == "1" ? true : nil
 
     @event = Event.find_by(id: params[:event_id]) if params[:event_id].present?
 
@@ -737,6 +745,7 @@ class AdminController < Admin::BaseController
     @wires = @wires.search_recipient(@q) if @q
 
     @wires = @wires.where(event_id: @event.id) if @event
+    @wires = @wires.where.missing(:reimbursement_payout_holding) if @exclude_reimbursements
 
     @wires = @wires.page(@page).per(@per).order(
       Arel.sql("aasm_state = 'pending' DESC"),
@@ -751,6 +760,7 @@ class AdminController < Admin::BaseController
     @q = params[:q].presence
     @event_id = params[:event_id].presence
     @status = WiseTransfer.aasm.states.collect(&:name).include?(params[:status]&.to_sym) ? params[:status] : nil
+    @exclude_reimbursements = params[:exclude_reimbursements] == "1" ? true : nil
 
     @event = Event.find_by(id: params[:event_id]) if params[:event_id].present?
 
@@ -760,6 +770,7 @@ class AdminController < Admin::BaseController
 
     @wise_transfers = @wise_transfers.where(event_id: @event_id) if @event_id
     @wise_transfers = @wise_transfers.where(aasm_state: @status) if @status
+    @wise_transfers = @wise_transfers.where.missing(:reimbursement_payout_holding) if @exclude_reimbursements
 
     @wise_transfers = @wise_transfers.page(@page).per(@per).order(
       Arel.sql("aasm_state = 'pending' DESC"),
