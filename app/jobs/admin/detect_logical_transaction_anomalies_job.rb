8a8c902b541b5455ee88bcb5aa916348d4d27147
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-06-23 16:07:41 -0400

    [Anomaly Job] Add event link and fix unknown variable (#13945)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Needed links to the events / ledgers.
    
    Also made an incorrect reference to event.
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Fixes bugs.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/jobs/admin/detect_logical_transaction_anomalies_job.rb
+++ app/jobs/admin/detect_logical_transaction_anomalies_job.rb
@@ -7,7 +7,7 @@ module Admin
     def perform
       hcb_codes = []
       HcbCode.where(event_id: 183).find_each do |hcb_code|
-        hcb_codes << event.id if hcb_code.amount_cents != hcb_code.ledger_item.amount_cents
+        hcb_codes << hcb_code.event.id if hcb_code.amount_cents != hcb_code.ledger_item.amount_cents
       end
 
       AdminMailer.logical_transaction_anomalies(hcb_codes: HcbCode.where(id: hcb_codes)).deliver_now
