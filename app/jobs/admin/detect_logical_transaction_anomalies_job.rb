669ccb0a29cf2cd69ae23732b574ad40c5a7fd63
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-06-24 08:48:02 -0400

    [Anomaly Job] Fix HCB code IDs (#13952)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    We were adding event IDs instead of HCB code IDs.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Add the correct ID.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/jobs/admin/detect_logical_transaction_anomalies_job.rb
+++ app/jobs/admin/detect_logical_transaction_anomalies_job.rb
@@ -7,7 +7,7 @@ module Admin
     def perform
       hcb_codes = []
       HcbCode.where(event_id: 183).where.not(ledger_item_id: nil).find_each do |hcb_code|
-        hcb_codes << hcb_code.event.id if hcb_code.amount_cents != hcb_code.ledger_item.amount_cents
+        hcb_codes << hcb_code.id if hcb_code.amount_cents != hcb_code.ledger_item.amount_cents
       end
 
       AdminMailer.logical_transaction_anomalies(hcb_codes: HcbCode.where(id: hcb_codes)).deliver_now
