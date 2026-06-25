f53fc553c436f2af2a6f295be9e821ff1a6d6f7f
Gary Tou <gary@garytou.com>
2026-06-25 13:04:13 -0400

    [PayoutMethod] Add callback to create LE PayoutMethod (#13979)
    ## Summary of the problem
    
    We want to backfill LE Payout Method, but we need to add a callback.
    
    
    ## Describe your changes
    
    Add callback. When User::PayoutMethod::* is created, also create the
    LegalEntity::PayoutMethod.
    
    ---------
    
    Co-authored-by: Samuel Fernandez <me@sfernandez.dev>

--- app/models/user/payout_method/shared.rb
+++ app/models/user/payout_method/shared.rb
@@ -19,6 +19,7 @@ class User
           Reimbursement::PayoutHolding.where(report: user.reimbursement_reports).failed.each(&:mark_settled!)
           Employee::Payment.where(employee: user.jobs).failed.each(&:mark_approved!)
         }
+        after_create_commit :create_legal_entity_payout_method
 
         validate do
           if User::PayoutMethod::UNSUPPORTED_METHODS.include?(self.class)
@@ -28,6 +29,21 @@ class User
 
         delegate :unsupported?, to: :class
         delegate :unsupported_details, to: :class
+
+        def create_legal_entity_payout_method
+          legal_entity = user.legal_entities.find_by(entity_type: :person)
+          return unless legal_entity
+
+          details_class = self.class.name.sub(/\AUser::/, "LegalEntity::").safe_constantize
+          return unless LegalEntity::PayoutMethod::ALL_METHODS.include?(details_class)
+
+          details = details_class.find_by(id:)
+          return unless details
+
+          LegalEntity::PayoutMethod.find_or_create_by!(legal_entity:, details:) do |payout_method|
+            payout_method.default = true
+          end
+        end
       end
 
       class_methods do
