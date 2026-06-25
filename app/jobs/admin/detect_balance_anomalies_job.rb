dff2712f015410c05650a246984656c93b67de6f
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-06-25 14:46:02 -0400

    [Balance Anomaly] Check if there is difference from sum of `smart_amount_cents` (#13981)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Previously we were only checking `balance_v2_cents`. We also want to
    detect anomalies from the sum of `smart_amount_cents`
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Check `event.ledger.balance_cents !=
    event.hcb_codes.sum(&:smart_amount_cents)`
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    ---------
    
    Co-authored-by: yodalightsabr <yoda@yodacode.xyz>

--- app/jobs/admin/detect_balance_anomalies_job.rb
+++ app/jobs/admin/detect_balance_anomalies_job.rb
@@ -7,7 +7,7 @@ module Admin
     def perform
       anomalous_events = []
       Event.find_each do |event|
-        anomalous_events << event.id if event.ledger.balance_cents != event.balance_v2_cents
+        anomalous_events << event.id if event.ledger.balance_cents != event.balance_v2_cents || event.ledger.balance_cents != event.hcb_codes.on_main_ledger.sum(&:smart_amount_cents)
       end
 
       AdminMailer.balance_anomalies(anomalous_events: Event.where(id: anomalous_events)).deliver_now
