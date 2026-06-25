38c223ef452bdbeff551aaddab3be88442f2995e
yodalightsabr <yoda@yodacode.xyz>
2026-06-25 08:36:22 -0700

    Check ledger items in anomaly script and exclude empty HcbCodes (#13970)
    ## Summary of the problem
    
    We want to also detect anomalies from the perspective of ledger items
    (similar to a full outer join). However, we also want to exclude
    HcbCodes without canonical transactions that are missing a ledger item.
    
    
    ## Describe your changes
    
    This PR includes ledger item anomalies in the script and excludes empty
    HCB codes.
    
    ---------
    
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/jobs/admin/detect_logical_transaction_anomalies_job.rb
+++ app/jobs/admin/detect_logical_transaction_anomalies_job.rb
@@ -6,11 +6,20 @@ module Admin
 
     def perform
       hcb_codes = []
-      HcbCode.where(event_id: 183).find_each do |hcb_code|
-        hcb_codes << hcb_code.id if hcb_code.ledger_item.nil? || hcb_code.smart_amount_cents != hcb_code.ledger_item.amount_cents
+      HcbCode.where(event_id:).find_each do |hcb_code|
+        hcb_codes << hcb_code.id if (hcb_code.ledger_item.nil? && hcb_code.no_transactions?) || hcb_code.smart_amount_cents != hcb_code.ledger_item.amount_cents
       end
 
-      AdminMailer.logical_transaction_anomalies(hcb_codes: HcbCode.where(id: hcb_codes)).deliver_now
+      ledger_items = []
+      Ledger::Item.where.not(id: HcbCode.where(event_id:).select(:ledger_item_id)).find_each do |ledger_item|
+        ledger_items << ledger_item.id
+      end
+
+      AdminMailer.logical_transaction_anomalies(hcb_codes: HcbCode.where(id: hcb_codes), ledger_items: LedgerItem.where(id: ledger_items)).deliver_now
+    end
+
+    def event_id
+      183
     end
 
   end
