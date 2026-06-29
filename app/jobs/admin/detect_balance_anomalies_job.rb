1f5d5a700e66360eb751db08a3eb6176acaff481
yodalightsabr <yoda@yodacode.xyz>
2026-06-29 07:54:29 -0700

    Fix detect balance anomalies job (#14038)
    ## Summary of the problem
    
    This job has a couple of bugs right now, and it takes a long time to
    run.
    
    
    ## Describe your changes
    
    Improve performance and fix bugs.

--- app/jobs/admin/detect_balance_anomalies_job.rb
+++ app/jobs/admin/detect_balance_anomalies_job.rb
@@ -7,10 +7,21 @@ module Admin
     def perform
       anomalous_events = []
       Event.find_each do |event|
-        anomalous_events << event.id if event.ledger.balance_cents != event.balance_v2_cents || event.ledger.balance_cents != event.hcb_codes.on_main_ledger.sum(&:smart_amount_cents)
+        if event.ledger.balance_cents != event.balance_v2_cents
+          anomalous_events << {
+            id: event.id,
+            slug: event.slug,
+            name: event.name,
+            balance_v2_cents: event.balance_v2_cents,
+            ledger_balance_cents: event.ledger.balance_cents
+          }
+          puts "Found anomaly on #{event.id}"
+        end
       end
 
-      AdminMailer.balance_anomalies(anomalous_events: Event.where(id: anomalous_events)).deliver_now
+      Rails.cache.write("event_balance_anomalies", anomalous_events)
+
+      AdminMailer.balance_anomalies(anomalous_events:).deliver_now
     end
 
   end
