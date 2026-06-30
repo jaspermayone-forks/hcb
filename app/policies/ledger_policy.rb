db41a6c69617fa408aa78860d19f5cd1f54376aa
yodalightsabr <yoda@yodacode.xyz>
2026-06-30 08:39:48 -0700

    Pass event into flipper (#14077)

--- app/policies/ledger_policy.rb
+++ app/policies/ledger_policy.rb
@@ -2,7 +2,7 @@
 
 class LedgerPolicy < ApplicationPolicy
   def show?
-    user&.auditor? || (Flipper.enabled?(:new_ledger_2026_06_30) && OrganizerPosition.role_at_least?(user, record.event, :reader))
+    user&.auditor? || (Flipper.enabled?(:new_ledger_2026_06_30, record.event) && OrganizerPosition.role_at_least?(user, record.event, :reader))
   end
 
 end
