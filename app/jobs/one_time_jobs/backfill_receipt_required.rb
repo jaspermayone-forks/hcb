29bff7476a5278d428b221771d3d464d355892e9
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-02 16:13:43 -0400

    [OTJ] Improve LO and add RR backfill jobs (#14150)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Makes LO backfill work on all orgs if there is no event id passed. Also
    adds a backfill OTJ for RR.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/jobs/one_time_jobs/backfill_receipt_required.rb
+++ app/jobs/one_time_jobs/backfill_receipt_required.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module OneTimeJobs
-  class BackfillLinkedObject < ApplicationJob
+  class BackfillReceiptRequired < ApplicationJob
     queue_as :metrics
 
     def perform(event_id: nil)
       ledger_items = event_id.nil? ? Ledger::Item.all : Event.find(event_id).ledger_items
 
       ledger_items.find_each do |item|
-        item.update!(linked_object: item.hcb_code.linked_object) unless item.hcb_code.linked_object.nil?
+        item.update!(receipt_required: item.calculate_receipt_required)
       end
     end
 
