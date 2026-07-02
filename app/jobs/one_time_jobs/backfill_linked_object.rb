f9c25300033f3992a8b3ef1a47080df691d6b175
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-02 16:31:52 -0400

    [OTJ] Fix unknown method in ledger column backfill jobs (#14152)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Undefined method error
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/jobs/one_time_jobs/backfill_linked_object.rb
+++ app/jobs/one_time_jobs/backfill_linked_object.rb
@@ -5,7 +5,7 @@ module OneTimeJobs
     queue_as :metrics
 
     def perform(event_id: nil)
-      ledger_items = event_id.nil? ? Ledger::Item.all : Event.find(event_id).ledger_items
+      ledger_items = event_id.nil? ? Ledger::Item.all : Event.find(event_id).ledger.items
 
       ledger_items.find_each do |item|
         item.update!(linked_object: item.hcb_code.linked_object) unless item.hcb_code.linked_object.nil?
