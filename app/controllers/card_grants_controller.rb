568de2d69bebf92747ef4a92044b04479ab7e094
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-13 14:13:40 -0400

    [CardGrant] Add new ledger with flipper (#14252)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    We needed the new ledger here.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Another place with the new ledger!
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->
    
    Co-authored-by: yodalightsabr <yoda@yodacode.xyz>

--- app/controllers/card_grants_controller.rb
+++ app/controllers/card_grants_controller.rb
@@ -40,7 +40,7 @@ class CardGrantsController < ApplicationController
     @per = params[:per] || 25
     @table_only = true
     @ledger = @event.ledger
-    @items = Ledger::Item.where(primary_mapping: Ledger::Mapping.where(ledger: Ledger.where(card_grant: @event.card_grants))).page(params[:page]).per(@per)
+    @items = Ledger::Item.where(primary_mapping: Ledger::Mapping.where(ledger: Ledger.where(card_grant: @event.card_grants))).order(datetime: :desc, created_at: :desc, id: :desc).page(params[:page]).per(@per)
   end
 
   def new
@@ -215,6 +215,11 @@ class CardGrantsController < ApplicationController
     @event = @card_grant.event
     @card = @card_grant.stripe_card
     @hcb_codes = @card_grant.visible_hcb_codes
+
+    @per = params[:per] || 25
+    @table_only = true
+    @ledger = @card_grant.ledger
+    @items = @card_grant.ledger.items.order(datetime: :desc, created_at: :desc, id: :desc).page(params[:page]).per(@per)
 
     @show_card_details = params[:show_details] == "true"
 
