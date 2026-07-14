8b58eef2bf5a6d09f77fe73e452e35130991c6ba
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-07-14 13:21:06 -0400

    [Ledger::Item] Improve `receipt_required` (#14276)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    There was inaccuracy.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Ignore expense payouts, stripe service fees, and bank fees.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/models/ledger/item.rb
+++ app/models/ledger/item.rb
@@ -124,7 +124,7 @@ class Ledger
     end
 
     def calculate_receipt_required
-      amount_cents < 0 && primary_ledger&.receipt_required? && linked_object_type != "Disbursement::Outgoing"
+      amount_cents < 0 && primary_ledger&.receipt_required? && !linked_object_type.in?(["Disbursement::Outgoing", "Reimbursement::ExpensePayout", "StripeServiceFee", "BankFee"])
     end
 
     def calculate_system_memo
