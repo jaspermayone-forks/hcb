da75390380e49ff44cc39ffe8a881ae81988b48d
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-08-10 17:38:32 -0400

    [Concern] Delete app/models/concerns/has_ledger_item.rb (#14563)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    This is no longer used.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Remove the file
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/models/concerns/has_ledger_item.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module HasLedgerItem
-  extend ActiveSupport::Concern
-
-  included do
-    has_one :ledger_item, class_name: "Ledger::Item", as: :linked_object
-  end
-end
