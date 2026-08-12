d8bfc4b4e1b0a33e875fa0dd96c6feeea1ec2785
Samuel Fernandez <me@sfernandez.dev>
2026-08-12 20:10:21 +0000

    Add Kairos Pathfinder plan (#14572)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    New partnership! 
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    New plan for subevents - contract already created on DocuSeal.
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

---
+++ app/models/event/plan/kairos_pathfinder.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: event_plans
+#
+#  id          :bigint           not null, primary key
+#  aasm_state  :string           not null
+#  inactive_at :datetime
+#  type        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  event_id    :bigint           not null
+#
+# Indexes
+#
+#  index_event_plans_on_event_id  (event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#
+class Event
+  class Plan
+    class KairosPathfinder < Standard
+      def label
+        "Kairos Pathfinder Program"
+      end
+
+      def contract_docuseal_template_id
+        5381274
+      end
+
+    end
+
+  end
+
+end
