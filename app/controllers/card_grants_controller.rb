ba153b24fc1a173b9182b396799bf3a66c02ba97
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-08-14 20:11:05 -0400

    [Card Grant Overview] Only order by `created_at` (#14621)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    Currently there is no indicator on the row of the pre authorization
    status so it is confusing to list them first.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Only order by `created_at`.
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/controllers/card_grants_controller.rb
+++ app/controllers/card_grants_controller.rb
@@ -24,11 +24,7 @@ class CardGrantsController < ApplicationController
     card_grants_page = (params[:page] || 1).to_i
     card_grants_per_page = (params[:per] || 20).to_i
 
-    @card_grants = @event.card_grants.includes(:disbursement, :user, :stripe_card, :pre_authorization, :subledger).order(
-      Arel.sql("card_grant_pre_authorizations.aasm_state='fraudulent' DESC"),
-      "card_grants.created_at DESC"
-    )
-    # we allow searching by purpose but sometimes the purpose shown in the table is actually the memo
+    @card_grants = @event.card_grants.includes(:disbursement, :user, :stripe_card, :pre_authorization, :subledger).order(created_at: :desc)
     @card_grants = @card_grants.search_for(params[:q]) if params[:q].present?
     @paginated_card_grants = @card_grants.page(card_grants_page).per(card_grants_per_page)
   end
