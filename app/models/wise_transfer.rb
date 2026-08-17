53409196cc0996c95e505606ab4467cd5761af31
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-08-17 14:06:51 -0400

    [Wise] Validate `usd_amount_cents` greater than 100 (#14638)
    ## Summary of the problem
    <!-- Why are these changes being made? What problem does it solve? Link
    any related issues to provide more details. -->
    We needed a standard minimum. When it is based on the destination's
    currency, this creates variances in the minimum amount cents.
    
    
    ## Describe your changes
    <!-- Explain your thought process to the solution and provide a quick
    summary of the changes. -->
    Validate `usd_amount_cents` instead of `amount_cents`
    
    
    <!-- If there are any visual changes, please attach images, videos, or
    gifs. -->

--- app/models/wise_transfer.rb
+++ app/models/wise_transfer.rb
@@ -73,8 +73,8 @@ class WiseTransfer < ApplicationRecord
   monetize :amount_cents, as: "amount", with_model_currency: :currency
   monetize :usd_amount_cents, as: "usd_amount", allow_nil: true
 
-  validates :amount_cents, numericality: { greater_than_or_equal_to: 100, message: "must be at least $1" }
-  validates :usd_amount_cents, numericality: { greater_than_or_equal_to: 0, message: "must be positive" }, allow_nil: true
+  validates :amount_cents, numericality: { greater_than_or_equal_to: 0, message: "must be positive" }
+  validates :usd_amount_cents, numericality: { greater_than_or_equal_to: 100, message: "must be at least $1" }, allow_nil: true
   validates :quoted_usd_amount_cents, numericality: { greater_than_or_equal_to: 0, message: "must be positive" }, allow_nil: true
 
   include PublicActivity::Model
