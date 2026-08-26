d7cf20f939d897b0858ac6564038293be4d967e7
yodalightsabr <yoda@yodacode.xyz>
2026-08-26 11:46:46 -0400

    Improve Ledger::AssertRequirementsJob and run nightly (#14733)
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/jobs/ledger/assert_requirements_job.rb
+++ app/jobs/ledger/assert_requirements_job.rb
@@ -10,16 +10,18 @@ class Ledger
 
     def perform(event_id: nil)
       @event = event_id.present? ? Event.find(event_id) : nil
-      @ledger_items = (@event&.ledger&.items || Ledger::Item.all).includes(:canonical_transactions, :canonical_pending_transactions, hcb_code: [:canonical_transactions, :canonical_pending_transactions])
-      @cts = (@event&.canonical_transactions || CanonicalTransaction.all).includes(:ledger_item)
-      @cpts = (@event&.canonical_pending_transactions || CanonicalPendingTransaction.all).includes(:ledger_item)
+      @ledger_items = (@event&.ledger&.items || Ledger::Item.all).includes(:canonical_transactions, :canonical_pending_transactions, hcb_code: [:canonical_transactions, :canonical_pending_transactions, :event, { subledger: [:card_grant] }])
+      @cts = (@event&.canonical_transactions || CanonicalTransaction.all).includes(:ledger_item, :canonical_event_mapping)
+      @cpts = (@event&.canonical_pending_transactions || CanonicalPendingTransaction.all).includes(:ledger_item, :canonical_pending_event_mapping)
       @anomalies = []
 
       cts_synced_with_hcb_code
       cpts_synced_with_hcb_code
-      event_synced_with_hcb_code
+      ledger_synced_with_hcb_code
       orphaned_cts
       orphaned_cpts
+      cems_match_ledger_mapping
+      cpems_match_ledger_mapping
 
       if @anomalies.any?
         Rails.error.report(FailedJobError.new("Ledger::AssertRequirementsJob failed with #{@anomalies.count} anomalies"))
@@ -50,12 +52,16 @@ class Ledger
       end
     end
 
-    def event_synced_with_hcb_code
+    def ledger_synced_with_hcb_code
       @ledger_items.find_each do |item|
         safely do
           hcb_code = item.hcb_code
-          if hcb_code.event.ledger != item.primary_ledger
-            report_anomaly "Ledger::Item #{item.hashid} ledger does not match HcbCode #{hcb_code.hashid} event ledger"
+          if (hcb_code.subledger&.card_grant || hcb_code.event)&.ledger != item.primary_ledger
+            report_anomaly "Ledger::Item #{item.hashid} ledger does not match HcbCode #{hcb_code.hashid} ledger"
+          end
+
+          if hcb_code.custom_memo.presence != item.custom_memo.presence
+            report_anomaly "Ledger::Item #{item.hashid} custom_memo does not match HcbCode #{hcb_code.hashid} custom_memo"
           end
         end
       end
@@ -76,6 +82,26 @@ class Ledger
         safely do
           if cpt.ledger_item.nil?
             report_anomaly "CanonicalPendingTransaction #{cpt.id} is orphaned (no Ledger::Item)"
+          end
+        end
+      end
+    end
+
+    def cems_match_ledger_mapping
+      @cts.find_each do |ct|
+        safely do
+          if (cem = ct.canonical_event_mapping) && (cem.subledger&.card_grant || cem.event)&.ledger != ct.ledger_item&.primary_ledger
+            report_anomaly "CanonicalTransaction #{ct.id} canonical_event_mapping does not match Ledger::Item"
+          end
+        end
+      end
+    end
+
+    def cpems_match_ledger_mapping
+      @cpts.find_each do |cpt|
+        safely do
+          if (cpem = cpt.canonical_pending_event_mapping) && (cpem.subledger&.card_grant || cpem.event)&.ledger != cpt.ledger_item&.primary_ledger
+            report_anomaly "CanonicalPendingTransaction #{cpt.id} canonical_pending_event_mapping does not match Ledger::Item"
           end
         end
       end
