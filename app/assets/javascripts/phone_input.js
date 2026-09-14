6fc30404e374d821838a79c84b81174b7784d574
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-09-14 18:12:47 +0000

    [Settings] Editing phone number causes weird bugs (#14970)
    ## Summary of the problem
    closes #14839 
    > Video summary: https://clips.garytou.com/s/1v1qe8thbzgjefm 
    >
    > Bugs in order of priority:
    > 1. Editing your phone number may cause you to verify your old phone
    number; instead of your new one
    > 2. The "phone number" field label is rendered weirdly when the form
    has a validation error
    > 3. Editing your phone number requires clicking save before you
    discover that you need to verify it.
    
    Overall just confusing UX.
    
    ## Describe your changes
    For 1st and 3rd: added a data-attribute to the input to get the
    persisted (saved) phone number; the javascript just compares this
    against the value of the input field and if it differs, hides the
    verification message (if present) and prompts the user to click save to
    update the phone number. Even if the number is already verified, the
    user is still prompted to save, which should slightly improve the user
    experience.
    
    For 2nd: just made the CSS selector more specific.
    
    <img width="593" alt="image"
    src="https://github.com/user-attachments/assets/0dc2ee9f-580b-47c1-b520-1f8b1419c1a5"
    />
    
    ---------
    
    Co-authored-by: Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>

--- app/assets/javascripts/phone_input.js
+++ app/assets/javascripts/phone_input.js
@@ -1,16 +1,34 @@
 ;(() => {
   const phoneInputField = document.querySelector('#phone_raw')
+  const phoneNumberField = document.querySelector('#phone_number')
+  
   const phoneInput = window.intlTelInput(phoneInputField, {
     initialCountry: 'us',
     preferredCountries: ['us', 'in', 'ca', 'sg', 'au', 'gb'],
     utilsScript:
       'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js'
   })
-  const callback = () => {
-    document.getElementById('phone_number').value = phoneInput.getNumber()
-    return true
+
+  const persistedPhoneNumber = phoneInputField.dataset.persistedPhoneNumber || ''
+
+  const updatePhoneNumber = () => {
+    phoneNumberField.value = phoneInput.getNumber()
+
+    const phoneNumberChanged = phoneNumberField.value !== persistedPhoneNumber
+    
+    const unverifiedMessage = document.querySelector('#unverified-phone-message')
+    const changedMessage = document.querySelector('#unsaved-phone-message')
+
+    if (unverifiedMessage) {
+      unverifiedMessage.style.display = phoneNumberChanged ? 'none' : ''
+    }
+
+    if (changedMessage) {
+      changedMessage.style.display = phoneNumberChanged ? '' : 'none'
+    }
   }
 
-  window.onsubmit = callback
-  phoneInputField.onblur = callback
+  window.onsubmit = updatePhoneNumber
+  phoneInputField.addEventListener('input', updatePhoneNumber)
+  phoneInputField.addEventListener('blur', updatePhoneNumber)
 })()
