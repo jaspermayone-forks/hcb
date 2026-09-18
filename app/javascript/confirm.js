fc022eac526ddfc46b1d5d73d7371e74bd002d9f
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2026-09-18 18:18:50 -0400

    Revert "Fix SweetAlert input disabling" (#15074)
    Reverts hackclub/hcb#15035
    
    This PR stopped turbo confirms from appearing entirely.

--- app/javascript/confirm.js
+++ app/javascript/confirm.js
@@ -1,5 +1,4 @@
 import swal from 'sweetalert'
-import $ from 'jquery'
 import { Turbo } from '@hotwired/turbo-rails'
 
 function showConfirm(
@@ -14,28 +13,12 @@ function showConfirm(
   }).then(v => !!v)
 }
 
-// jquery-ujs disables `data-disable-with` elements shortly after every submit
-// event; when the confirmation aborts the submission, nothing re-enables them.
-function reenableFormElements(formElement) {
-  if (!formElement || !$.rails) return
-
-  $(formElement)
-    .find($.rails.enableSelector)
-    .each((_, element) => {
-      if ($(element).data('ujs:disabled')) $.rails.enableFormElement($(element))
-    })
-}
-
-Turbo.config.forms.confirm = async (message, formElement, submitter) => {
+Turbo.config.forms.confirm = (message, formElement, submitter) => {
   const dangerMode = Boolean(
     submitter?.hasAttribute('data-turbo-confirm-danger') ||
     formElement?.hasAttribute('data-turbo-confirm-danger')
   )
-  const confirmed = await showConfirm(message, { dangerMode })
-
-  if (!confirmed) reenableFormElements(formElement)
-
-  return confirmed
+  return showConfirm(message, { dangerMode })
 }
 window.showConfirm = showConfirm
 window.swal = swal
